d901c1d316aee28d170f8b277e9339937f4bf845
Djalma-Neto <djalma7743@gmail.com>
2022-02-13 18:20:48 -0300

    .

--- SERVO/src/main.cpp
+++ SERVO/src/main.cpp
@@ -4,40 +4,59 @@
 const int SERVO = 13;
 const int TOUCH = 12;
 
-int minUs = 1000;
-int maxUs = 2000;
 int value = 0;
+
+int timePrint = 0;
 
 Servo myservo;
 ESP32PWM pwm;
 
-void setup() {
-  Serial.begin(9600);
-}
 
-void loop() {
-  myservo.attach(SERVO);
-
-  int val = digitalRead(TOUCH);
-
-  if(val){
-    if(value == 0){
-      value = 180;
-      myservo.write(value);
-    }else{
-      value = 0;
-      myservo.write(value);
-    }
-  }
-
-  delay(1000);
-  printar(val, value);
-}
-
-void printar(int val, int value){
+void printar(int val, int value, int valueRead){
   Serial.print("val: ");
   Serial.println(val);
 
   Serial.print("value: ");
   Serial.println(value);
+
+  Serial.print("valueRead: ");
+  Serial.println(valueRead);
+  Serial.println("--------------------");
+}
+
+void printServo(int val){
+  int X = 100;
+  int Y = 80;
+
+  if(value == X){
+    value = Y;
+    myservo.write(value);
+    delay(1000);
+    myservo.write(90);
+  } else {
+    value = X;
+    myservo.write(value);
+    delay(1140);
+    myservo.write(90);
+  }
+
+  int valueRead = myservo.read();
+  printar(val, value, valueRead);
+}
+
+void setup() {
+  Serial.begin(9600);
+  myservo.attach(SERVO);
+  myservo.write(90);
+}
+
+void loop() {
+  int val = digitalRead(TOUCH);
+  
+  if(millis()-timePrint == 1000){
+    if(val){
+      printServo(val);
+    }
+    timePrint = millis();
+  }
 }
